5eb2ecc4b3378dd800ff9049f1b5e7f698d07035
Joiarib <joi.heyer@gmail.com>
2023-02-18 22:23:37 -0500

    Added a javascript function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed * 0.621371);
+  let feelsLike = document.querySelector("#feelsLike");
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+}
+
+let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
