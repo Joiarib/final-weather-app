64fe620d9bdbe05ed1a8ff8a17be8140d854432f
Joiarib <joi.heyer@gmail.com>
2023-02-22 21:34:27 -0500

    moved forecast HTML into Javascript and added forecast function

--- src/index.js
+++ src/index.js
@@ -20,6 +20,52 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[days];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row>`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temp">
+        <span class="maximum-temp">${math.round(forecastDay.temp.max)}°</span>
+        <span class="minimum-temp"> ${Math.round(forecastDay.temp.min)}° </span>
+      </div>
+    
+  </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 function displayTemperature(response) {
   console.log(response);
   fahrenheitTemp = Math.round(response.data.main.temp);
@@ -38,6 +84,7 @@ function displayTemperature(response) {
   let feelsLike = document.querySelector("#feelsLike");
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   let iconElement = document.querySelector("#icon");
+
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -70,6 +117,7 @@ function fahrenheitConversion(event) {
 
 let fahrenheitTemp = null;
 search("Boston");
+displayForecast();
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchSubmit);
 let celsiusLink = document.querySelector("#celsius-link");
